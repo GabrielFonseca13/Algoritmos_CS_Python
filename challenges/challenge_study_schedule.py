a13c92804622d281f018238fc5ca1226ed78510c
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-06-13 11:05:40 -0300

    fix req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -9,19 +9,3 @@ def study_schedule(permanence_period, target_time):
         if student[0] <= target_time and student[1] >= target_time:
             counter += 1
     return counter
-
-
-print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 2))
-
-# 1.2 - Retorne None se em permanence_period houver alguma entrada inválida;
-
-# 1.3 - Retorne None se target_time recebe um valor vazio;
-
-# 1.4 - A função deverá, por meio de análise empírica, se comportar (no
-# avaliador remoto em sua Pull Request) como no máximo O(n), ou seja, com
-# complexidade assintótica linear.
-
-# # Nos arrays temos 6 estudantes
-
-# # estudante             1       2       3       4       5       6
-# permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
