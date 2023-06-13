70ee6c148d584a61b771d2f7684fc34d517caf3d
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-06-13 10:17:57 -0300

    [Dev] Gabriel Fonseca

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,20 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
     raise NotImplementedError
+
+
+# 1.1 - Retorne a quantidade de estudantes presentes para uma entrada
+# específica;
+
+# 1.2 - Retorne None se em permanence_period houver alguma entrada inválida;
+
+# 1.3 - Retorne None se target_time recebe um valor vazio;
+
+# 1.4 - A função deverá, por meio de análise empírica, se comportar (no
+# avaliador remoto em sua Pull Request) como no máximo O(n), ou seja, com
+# complexidade assintótica linear.
+
+# # Nos arrays temos 6 estudantes
+
+# # estudante             1       2       3       4       5       6
+# permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
