f578017c07e49dfef00b9009916b6f5e159b184b
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-06-13 13:48:18 -0300

    req 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,21 +1,28 @@
+def bubble_sort(string):
+    n = len(string)
+
+    for i in range(n - 1):
+        for j in range(n - 1 - i):
+            if string[j] > string[j + 1]:
+                string[j], string[j + 1] = string[j + 1], string[j]
+
+    return string
+
+
 def is_anagram(first_string, second_string):
-    if first_string == "" or second_string == "":
-        return first_string, second_string, False
+    first_string = first_string.lower()
+    second_string = second_string.lower()
 
-    return True
+    first_string_list = list(first_string)
+    second_string_list = list(second_string)
 
+    sorted_first_string = bubble_sort(first_string_list)
+    sorted_second_string = bubble_sort(second_string_list)
 
-# 4.1 - Retorne True se as palavras passadas por parâmetro forem anagramas;
+    is_anagram = sorted_first_string == sorted_second_string
 
-# 4.2 - Retorne False se as palavras passadas por parâmetro não forem
-# anagramas;
-
-# 4.3 - Retorne False se alguma das palavras passadas por parâmetro for uma
-# string vazia;
-
-# 4.4 - A função deverá, por meio de análise empírica, se comportar
-# (no avaliador remoto em sua Pull Request) como no máximo O(n log n), ou seja,
-# com complexidade assintótica linearítmica.
-
-# 4.5 - Retorne True se as palavras passadas forem anagramas sem diferenciar
-# maiúsculas e minúsculas.
+    return (
+        "".join(sorted_first_string),
+        "".join(sorted_second_string),
+        is_anagram,
+    )
